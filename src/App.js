55255606bd93514e097d1ff4d884f9d883eea330
Dima M <54527086+dmitry-tupelo@users.noreply.github.com>
2023-09-09 00:29:22 +0300

    change to MUI

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import Quiz from './Quiz';
-import { Box, Container } from '@radix-ui/themes';
+import React from 'react'
+import { Box, Container } from '@mui/material'
+import Quiz from './Quiz'
 
-function App() {
+function App () {
   return (
-    <Box style={{ background: 'var(--gray-a2)', borderRadius: 'var(--radius-3)', height: "100vh", alignItems: "center", justifyContent: "center", display: "flex" }}>
+    <Box>
       <Container size="1">
         <Box>
           <Quiz />
         </Box>
       </Container>
     </Box>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Quiz.js
+++ src/Quiz.js
@@ -1,108 +1,112 @@
-import React, {useEffect, useState} from 'react';
-import { Flex, RadioGroup, Text, Heading, Button, Box } from '@radix-ui/themes';
+import React, { useEffect, useState } from 'react'
+import { Button, Box, Typography, RadioGroup } from '@mui/material'
+import { Circles } from 'react-loader-spinner'
+import { generateRandomQuestionId } from './utils/utils'
+import RadioButton from './components/Radio/RadioButton'
 
 const Quiz = () => {
-    const quizJson = require('./quiz.json');
-    const [quizData, setQuizData] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [selectedOption, setSelectedOption] = useState(null);
-    const [answersCount, setAnswersCount] = useState(0);
-    const [randomNumber, setRandomNumber] = useState();
-    const [correct, setCorrect] = useState(null)
+  const quizJson = require('./quiz.json')
+  const [quizData, setQuizData] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [selectedOption, setSelectedOption] = useState(null)
+  const [answersCount, setAnswersCount] = useState(0)
+  const [randomNumber, setRandomNumber] = useState()
+  const [correct, setCorrect] = useState(null)
 
-    const generateRandomNumber = () => Math.floor(Math.random() * quizData.length);
-    useEffect(() => {
-        setRandomNumber(generateRandomNumber())
-        setQuizData(quizJson);
-        setLoading(false);
-    },[])
+  useEffect(() => {
+    setQuizData(quizJson)
+    setRandomNumber(generateRandomQuestionId(quizData.length))
+    setLoading(false)
+  }, [])
 
-        
-    console.log(correct)
-
-    const renderRandomQuestion = (quizItem) => {
-        const renderAnswers = (options) =>{
-            return options.map((answer, index) => {
-                return (
-                    <label key={answer} onClick={() => setSelectedOption(index)} htmlFor={index}>
-                        <Flex gap="2" align="center">
-                            <RadioGroup.Item value="1" id={index} checked={selectedOption === index} />
-                            <Text size="2">{answer}</Text>
-                        </Flex>
-                        
-                        
-                    </label>
-                )
-            })
-        }
-
-        const checkAnswer = () => {
-            if(selectedOption === quizItem.answer){
-                setCorrect(true)
-                setAnswersCount((prev) => prev + 1)
-            } else {
-                setCorrect(false);
-            }
-            setSelectedOption(null)
-        }
+  const renderRandomQuestion = (quizItem) => {
+    const renderAnswers = (options) => {
+      return options.map((answer, index) => {
         return (
+          <RadioButton setSelectedOption={setSelectedOption} key={index} answer={answer} index={index} />
+        )
+      })
+    }
+
+    const checkAnswer = () => {
+      console.log(selectedOption)
+      console.log(quizItem.answer)
+      if (selectedOption === quizItem.answer) {
+        console.log('CORRECT')
+        console.log(answersCount)
+        setCorrect(true)
+        setAnswersCount((prev) => prev + 1)
+        console.log(answersCount)
+      } else {
+        console.log('ELSE')
+        setCorrect(false)
+      }
+
+      setSelectedOption(null)
+      setTimeout(() => {
+        setCorrect(null)
+        setRandomNumber(generateRandomQuestionId(quizData.length))
+      }, 2000)
+    }
+    return (
             <div>
                 <Box pb="5">
-                    <Heading>
-                        {quizItem?.question}
-                    </Heading>
+                  <Typography component="h2">
+                    {quizItem?.question}
+                  </Typography>
                 </Box>
                 <Box pb="4">
                     <Box pb="4">
-                        <RadioGroup.Root>
-                            <Flex gap="2" direction="column">
-                                
+                    <RadioGroup
+                        aria-labelledby="demo-radio-buttons-group-label"
+                        defaultValue="female"
+                        name="radio-buttons-group"
+                      >
                                 {renderAnswers(quizItem?.options)}
-                            </Flex>
-                        </RadioGroup.Root>
+                      </RadioGroup>
                     </Box>
                     {correct !== null && (
-                        <Flex mb="2">
-                            <Text>
+                            <Typography component={'h3'}>
                                 Ваш ответ - {correct ? '✅' : '❌'}
-                            </Text>
-                        </Flex>
+                            </Typography>
                     )}
-                    <Flex  gap="1">
                         <Button
+                            disabled={correct !== null || selectedOption === null}
                             mr={2}
                             onClick={checkAnswer}
                         >
-                            Проверить ответ
+                            {correct !== null
+                              ? <Circles
+                                  color="#000"
+                                  height="15"
+                                  width="15"
+                                  ariaLabel="circles-loading"
+                                  wrapperStyle={{}}
+                                  wrapperClass=""
+                                  visible={true}
+                                />
+                              : 'Проверить ответ'}
                         </Button>
-                        <Button
-                            onClick={() => {
-                                setCorrect(null);
-                                setRandomNumber(generateRandomNumber())
-                            }}
-                        >
-                            Дальше
-                        </Button>
-                    </Flex>
-                
+
                 </Box>
                 <div>
                     Правильных ответов: {answersCount}
                 </div>
             </div>
-        )
-    }
+    )
+  }
 
-    return (
-        <Flex>
-             {loading ? 
-                (
-                    <Heading>Loading</Heading>
-                ) : (
+  return (
+        <div>
+             {loading
+               ? (
+                    <Typography>Loading</Typography>
+                 )
+               : (
                     <div>{renderRandomQuestion(quizData[randomNumber])}</div>
-                )}
-        </Flex>
-    );
+                 )}
+        </div>
+  )
 }
 
-export default Quiz;
+export default Quiz
